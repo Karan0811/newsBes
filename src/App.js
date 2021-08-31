67e2e51d60c5428802d124bada80682c78d004f3
Karan0811 <46247163+Karan0811@users.noreply.github.com>
2021-08-31 11:30:36 +0530

    react router added and category

--- src/App.js
+++ src/App.js
@@ -3,13 +3,47 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <NavBar/>
-        <News/>
+        <Switch>
+          <Route exact path="/business">
+          <News key = 'business' pageSize = {10} category = 'business'/>
+          </Route>
+          <Route exact path="/technology">
+          <News key = 'technology' pageSize = {10} category = 'technology'/>
+          </Route>
+          <Route exact path="/sports">
+          <News key = 'sports' pageSize = {10} category = 'sports'/>
+          </Route>
+          <Route exact path="/general">
+          <News key = 'general' pageSize = {10} category = 'general'/>
+          </Route>
+          <Route exact path="/science">
+          <News key = 'science' pageSize = {10} category = 'science'/>
+          </Route>
+          <Route exact path="/health">
+          <News key = 'health' pageSize = {10} category = 'health'/>
+          </Route>
+          <Route exact path="/entertainment">
+          <News key = 'entertainment' pageSize = {10} category = 'entertainment'/>
+          </Route>
+          {/* <Route exact path="/technology">
+          <News key = 'technology' pageSize = {10} category = 'technology'/>
+          </Route> */}
+        </Switch>
       </div>
+      </Router>
+
     )
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,8 +6,8 @@ export default class NewsItem extends Component {
 
     return (
       <div>
-        <div className='card' style={{ width: '18rem' }}>
-          <img src={imageUrl} className='card-img-top' alt='...' />
+        <div className='card'>
+          <img src={imageUrl ? imageUrl : "https://images.moneycontrol.com/static-mcnews/2020/01/BSE_Sensex_Stocks_market-770x433.png"} className='card-img-top' alt='...' />
           <div className='card-body'>
             <h5 className='card-title'>{title}</h5>
             <p className='card-text'>{description}</p>
