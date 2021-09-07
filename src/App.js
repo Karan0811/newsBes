ff6d1be9143490cea30fdb5acc327998c8e7bac8
Karan0811 <46247163+Karan0811@users.noreply.github.com>
2021-09-07 10:02:55 +0530

    Class to function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
 import LoadingBar from 'react-top-loading-bar'
@@ -11,17 +11,11 @@ import {
   Switch,
   Route} from "react-router-dom";
 
-export default class App extends Component {
-  apiKey = env.key;
-  pageSize = 20;
-   state = {
-    progress:0
-  }
+const App = () => {
+  const apiKey = env.key;
+  const pageSize = 20;
+  const [progress, setProgress] = useState(0)
 
-  setProgress = (progress)=>{
-    this.setState({progress: progress})
-  }
-  render() {
     return (
       <Router>
       <div>
@@ -29,43 +23,42 @@ export default class App extends Component {
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress} 
+        progress={progress} 
       />
         <Switch>
         <Route exact path="/">
-          <News setProgress = {this.setProgress} key = 'general' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'general'/>
+          <News setProgress = {setProgress} key = 'general' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'general'/>
           </Route>
           <Route exact path="/business">
-          <News setProgress = {this.setProgress} key = 'business' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'business'/>
+          <News setProgress = {setProgress} key = 'business' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'business'/>
           </Route>
           <Route exact path="/technology">
-          <News setProgress = {this.setProgress} key = 'technology' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'technology'/>
+          <News setProgress = {setProgress} key = 'technology' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'technology'/>
           </Route>
           <Route exact path="/sports">
-          <News setProgress = {this.setProgress} key = 'sports' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'sports'/>
+          <News setProgress = {setProgress} key = 'sports' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'sports'/>
           </Route>
           <Route exact path="/general">
-          <News setProgress = {this.setProgress} key = 'general' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'general'/>
+          <News setProgress = {setProgress} key = 'general' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'general'/>
           </Route>
           <Route exact path="/science">
-          <News setProgress = {this.setProgress} key = 'science' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'science'/>
+          <News setProgress = {setProgress} key = 'science' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'science'/>
           </Route>
           <Route exact path="/health">
-          <News setProgress = {this.setProgress} key = 'health' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'health'/>
+          <News setProgress = {setProgress} key = 'health' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'health'/>
           </Route>
           <Route exact path="/entertainment">
-          <News setProgress = {this.setProgress} key = 'entertainment' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'entertainment'/>
+          <News setProgress = {setProgress} key = 'entertainment' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'entertainment'/>
           </Route>
           {/* <Route exact path="/technology">
-          <News setProgress = {this.setProgress} key = 'technology' pageSize = {this.pageSize} apiKey = {this.apiKey}   country = 'in' category = 'technology'/>
+          <News setProgress = {setProgress} key = 'technology' pageSize = {pageSize} apiKey = {apiKey}   country = 'in' category = 'technology'/>
           </Route> */}
         </Switch>
       </div>
       </Router>
 
     )
-  }
 }
 
 
-// export default App;
+export default App;
